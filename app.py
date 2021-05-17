f7bfc4646bdc75e3a2507104c1471ba8d9ba677d
sa <psa5013@hanmail.com>
2021-05-17 12:42:21 +0900

    get, post 파라미터값 가져오기

--- app.py
+++ app.py
@@ -7,12 +7,24 @@ def hello():
 
 
 @app.route('/test', methods=['GET', 'POST']) 
-def testRoute(): 
-    if request.method == 'GET': 
-        return "GET으로 전달" 
-    else: 
-        return "POST로 전달"
+def testRouter(): 
+    if request.method == 'GET':
 
+        # [Get-파라미터값 가져오기]
+        #  get방식으로 url?num=10&name=홍길동   전달 시,
+
+        # args_dict = request.args.to_dict()        # dict형태(key,value)로 나타남
+        num = request.args["num"]                   # 10
+        name = request.args.get("name")             # 홍길동
+
+        return "GET으로 전달된 데이터({}, {})".format(num, name)
+
+
+    else: 
+        #  [POST-파라미터값 가져오기]
+        num = request.form["num"] 
+        name = request.form["name"]
+        return "POST로 전달된 데이터({}, {})".format(num, name)
 
 
 
@@ -30,3 +42,5 @@ if __name__ == '__main__':
     app.run(debug=True)         
     #  debug=True를  넣으면 route 추가 시 마다 재시작 안해도 된다.(없을경우 재시작 필요)
     #  app.debug = True 또는 app.run(debug=True)  -> 둘다 같은 작용
+
+
